d09bfc19540897b16f9a066a928fcc13ac5df393
thegreatgatsby1998 <hephzibahokeowo@gmail.com>
2022-06-08 05:05:51 -0700

    j

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,20 +6,20 @@
  *     *
  *      * Return: return the integer value 0
  *       *
- *        */
+ */
 int main(void)
 {
-		int num;
-			char alphabet;
+int num;
+char alphabet;
 
-				for (num = 0; num < 10; num++)
-						{
-									putchar(num + '0');
-										}
-					for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
-							{
-										putchar(alphabet);
-											}
-						putchar('\n');
-							return (0);
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
