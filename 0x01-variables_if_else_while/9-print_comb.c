80b33a1fa52799b0f9ccd3c4f3a857c0a10fe4b8
thegreatgatsby1998 <hephzibahokeowo@gmail.com>
2022-06-08 05:14:03 -0700

    ll

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,21 +6,21 @@
  *     *
  *      * Return: return the integer value 0
  *       *
- *        */
+ */
 int main(void)
 {
-		int num;
+int num;
 
-			for (num = 0; num < 10; num++)
-					{
-								putchar(num + '0');
-										if (num < 9)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+putchar(' ')
+}
+}
+putchar('\n');
+return (0);
 }
 
