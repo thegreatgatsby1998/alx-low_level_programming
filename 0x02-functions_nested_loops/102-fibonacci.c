20c81082ef5ea329cafe9aa3af65ea4b2bffe795
thegreatgatsby1998 <hephzibahokeowo@gmail.com>
2022-06-08 10:58:31 -0700

    cc

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,21 @@
  */
 int main(void)
 {
-unsigned int a, b, c, d, s;
+long int a, b, c, d;
 
 a = 1;
 b = 2;
 c = 3;
-s = 2;
-for (d = 2; d <= 32; d++)
-{					 
-if (c % 2 == 0)
-s = s + c;
+
+printf("%ld, ", a);
+printf("%ld, ", b);
+for (d = 3; d < 50; d++)
+{
+printf("%ld, ", c);
 a = b;
 b = c;
 c = a + b;
 }
-printf("%u\n", s);
+printf("%ld\n", c);
 return (0);
 }
