0c75400a5dff52c46f59a2eac359fdd05aac4bea
thegreatgatsby1998 <hephzibahokeowo@gmail.com>
2022-06-08 17:49:11 -0700

    pop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,15 @@
  *     *
  *      *Return: return integer value o
  *       *
- *        */
+ */
 int main(void)
 {
-		int alphabet;
+int alphabet;
 
-			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-					{
-								putchar(alphabet);
-									}
-				putchar('\n');
-					return (0);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
