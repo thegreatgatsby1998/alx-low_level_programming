d72a1de2ef8015c32ab5eed10c18ef6be20cae0a
thegreatgatsby1998 <hephzibahokeowo@gmail.com>
2022-06-08 17:52:51 -0700

    pop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,24 +6,24 @@
  *     *
  *      * Return: return the integer value 0
  *       *
- *        */
+ */
 int main(void)
 {
-		int i;
-			int j;
+int i;
+int j;
 
-				for (i = 0; i < 9; i++)
-						{
-									for (j = i + 1; j < 10; j++)
-												{
-																putchar(i + '0');
-																			putchar(j + '0');
-																						if (i == 8 && j == 9)
-																											break;
-																									putchar(',');
-																												putchar(' ');
-																														}
-										}
-					putchar('\n');
-						return (0);
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i == 8 && j == 9)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
